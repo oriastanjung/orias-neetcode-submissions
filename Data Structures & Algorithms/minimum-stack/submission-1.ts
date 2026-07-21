00edc03146315d195ee45374c2f33d4b54ee7111
O. Riastanjung <87843869+oriastanjung@users.noreply.github.com>
2026-07-21 16:41:21 +0700

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.ts
@@ -0,0 +1,35 @@
+class MinStack {
+    stack : number[] = []
+
+    constructor() {
+    }
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val: number): void {
+        this.stack.push(val)
+    }
+
+    /**
+     * @return {void}
+     */
+    pop(): void {
+        this.stack.pop()
+        
+    }
+
+    /**
+     * @return {number}
+     */
+    top(): number {
+        return this.stack[this.stack.length - 1]
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin(): number {
+        return Math.min(...this.stack)
+    }
+}
